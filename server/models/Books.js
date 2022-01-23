b5504faa554eacad04622315d7ba5bcbf8a7c401
sdesire <sdesire@ltu.edu>
2022-01-23 18:25:49 -0500

    server side models

---
+++ server/models/Books.js
@@ -0,0 +1,31 @@
+const { Schema } = require("mongoose");
+
+// This is a subdocument schema
+const bookSchema = new Schema({
+  authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved book id from GoogleBooks
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = bookSchema;
